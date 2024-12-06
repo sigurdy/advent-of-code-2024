7110f264f28b4e45f4531eecd069db84a999f486
sigurdy <131282266+sigurdy@users.noreply.github.com>
2024-12-06 18:56:03 +0100

    prepare day7

---
+++ AdventOfCode/Solutions/Day7/Day7.cs
@@ -0,0 +1,23 @@
+namespace AdventOfCode.Solutions.Day7;
+
+public class Day7 : Solutions
+{
+    public Day7() : base(7)
+    {
+    }
+
+    public override void Run()
+    {
+        throw new NotImplementedException();
+    }
+
+    public override int RunPart1(string[] inputLines)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override int RunPart2(string[] inputLines)
+    {
+        throw new NotImplementedException();
+    }
+}
