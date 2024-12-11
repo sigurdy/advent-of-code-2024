8c9dca8891cde9e6d9ae340427231e5a7e4c1d5d
sigurdy <131282266+sigurdy@users.noreply.github.com>
2024-12-11 16:31:53 +0100

    continue

--- AdventOfCode/Solutions/Day9/Day9.cs
+++ AdventOfCode/Solutions/Day9/Day9.cs
@@ -21,6 +21,15 @@ public class Day9 : Solutions
         }
         Console.WriteLine();
     }
+    
+    private void PrintAllBatches(List<Batch> batches)
+    {
+        foreach (var batch in batches)
+        {
+            batch.PrintBatch();
+        }
+        Console.WriteLine();
+    }
 
     private List<int> FragmentData(List<Batch> batches)
     {
@@ -40,7 +49,7 @@ public class Day9 : Solutions
         int indexPointer = 0;
         int numberPointer = array.Count - 1;
 
-        while(numberPointer > indexPointer)
+        while (numberPointer > indexPointer)
         {
             // Swich indexes
             if (output[indexPointer] == -1 && output[numberPointer] != -1)
@@ -50,7 +59,7 @@ public class Day9 : Solutions
 
                 output[indexPointer] = rightNumber;
                 output[numberPointer] = leftNumber;
-                
+
                 indexPointer++;
                 numberPointer--;
 
@@ -61,6 +70,7 @@ public class Day9 : Solutions
             {
                 numberPointer--;
             }
+
             if (output[indexPointer] != -1)
             {
                 indexPointer++;
@@ -69,27 +79,29 @@ public class Day9 : Solutions
 
         return output;
     }
-    
+
     private List<Batch> DefragmentationPart2(List<Batch> batches)
     {
         List<Batch> newBatches = new List<Batch>(batches);
-        
-        int batchIdToAdd = batches.First().Id;
-        int batchIdToMove = batches.Last().Id;
 
-        while(batchIdToMove > batchIdToAdd)
+        Batch sourceBatch = newBatches.Last();
+        while (sourceBatch.Id > 0)
         {
-            Batch lastMovableBatch = newBatches.Where(x => x.Id == batchIdToMove).First();
             Batch? replacableBatch = newBatches
-                .Where(x => x.FreeSpace >= lastMovableBatch.Files) // the batch has enough free space
-                .OrderBy(x => x.Id) // select the IDs; 
-                .FirstOrDefault();// get the smallest id
-            if(replacableBatch == null)
+                .Where(x => x.FreeSpace >= sourceBatch.Files.Length) // the batch has enough free space
+                .OrderBy(x => x.Id) // order by id 
+                .FirstOrDefault(); // get the smallest id
+            if (replacableBatch == null)
             {
-                batchIdToAdd++;
+                var nextBatch = newBatches.FirstOrDefault(x => x.Id == sourceBatch.Id - 1);
+                if (nextBatch == null) break;
+                sourceBatch = nextBatch;
                 continue;
             }
 
+            replacableBatch.InsertNumbers(sourceBatch.Files);
+            newBatches.Remove(sourceBatch);
+            sourceBatch = newBatches.Last();
         }
 
         return newBatches;
@@ -103,17 +115,18 @@ public class Day9 : Solutions
         for (int i = 0; i < input.Length; i += 2)
         {
             int files = int.Parse(input[i].ToString());
-            if (i+1 >= input.Length)
+            if (i + 1 >= input.Length)
             {
                 int[] lastNumber = [.. Enumerable.Repeat(id, files).ToArray()];
                 batches.Add(new Batch(id, lastNumber));
                 break;
             }
-            int free = int.Parse(input[i+1].ToString());
 
-            int[] numbers = [ .. Enumerable.Repeat(id, files).ToArray(), .. Enumerable.Repeat(-1, free).ToArray() ];
+            int free = int.Parse(input[i + 1].ToString());
 
-            batches.Add( new Batch(id, numbers) );
+            int[] numbers = [.. Enumerable.Repeat(id, files).ToArray(), .. Enumerable.Repeat(-1, free).ToArray()];
+
+            batches.Add(new Batch(id, numbers));
             id++;
         }
 
@@ -123,7 +136,7 @@ public class Day9 : Solutions
     private long CalculateChecksum(List<int> data)
     {
         long sum = 0;
-        for (int i = 0; i < data.Count; i++) 
+        for (int i = 0; i < data.Count; i++)
         {
             long id = i;
             long currentNumber = data[i];
@@ -131,17 +144,19 @@ public class Day9 : Solutions
             {
                 break;
             }
+
             long localSum = id * currentNumber;
             sum += localSum;
         }
+
         return sum;
     }
 
     public override long RunPart1(string[] inputLines)
     {
         string input = inputLines[0];
-        List<Batch> bactches = GenerateBatches(input);
-        List<int> fragmentedData = FragmentData(bactches);
+        List<Batch> batches = GenerateBatches(input);
+        List<int> fragmentedData = FragmentData(batches);
         List<int> defragmentedData = DefragmentationPart1(fragmentedData);
         //PrintData(fragmentedData);
         //PrintData(defragmentedData);
@@ -153,9 +168,10 @@ public class Day9 : Solutions
     public override long RunPart2(string[] inputLines)
     {
         string input = inputLines[0];
-        List<Batch> bactches = GenerateBatches(input);
-        List<int> fragmentedData = FragmentData(bactches);
-        List<int> defragmentedData = DefragmentationPart2(bactches);
+        List<Batch> batches = GenerateBatches(input);
+        List<Batch> defragmentedData = DefragmentationPart2(batches);
+        PrintAllBatches(defragmentedData);
+        
         return 0;
     }
 }
@@ -163,20 +179,69 @@ public class Day9 : Solutions
 public class Batch
 {
     public int Id { get; }
-    public int FreeSpace { get; }
-    public int Files { get; }
-    public int[] Numbers { get; }
+    public int FreeSpace { get; private set; }
+    public int[] Files { get; private set; }
+    public int[] Numbers { get; private set; }
 
     public Batch(int id, int[] numbers)
     {
         Id = id;
         Numbers = numbers;
-        FreeSpace = numbers.Count(x => x == -1);
-        Files = numbers.Count(x => x != -1);
+        FreeSpace = GetFreeSpace();
+        Files = GetFiles();
+    }
+
+    public void PrintBatch()
+    {
+        foreach (var number in Numbers)
+        {
+            if (number == -1)
+            {
+                Console.Write(".");
+            }
+            else
+            {
+                Console.Write(number);
+            }
+        }
     }
 
     public void UpdateBatch(int[] numbers)
     {
+        Numbers = numbers;
+        FreeSpace = GetFreeSpace();
+        Files = GetFiles();
+    }
 
+    public void InsertNumbers(int[] numbers)
+    {
+        if (numbers.Length > FreeSpace) throw new IndexOutOfRangeException("Cannot insert more numbers!");
+        int[] newNumbers = (int[])Numbers.Clone();
+
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            for (int j = 0; j < newNumbers.Length; j++)
+            {
+                if (newNumbers[j] != -1)
+                {
+                    continue;
+                }
+                newNumbers[j] = numbers[i];
+                break;
+            }
+        }
+        
+        Numbers = newNumbers;
+        FreeSpace = GetFreeSpace();
+        Files = GetFiles();
+    }
+
+    private int GetFreeSpace()
+    {
+        return Numbers.Count(x => x == -1);
+    }
+    private int[] GetFiles()
+    {
+        return Numbers.Where(x => x != -1).ToArray();
     }
 }
